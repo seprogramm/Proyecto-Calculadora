c182973743e8bdeefbb463a57351be91bef3d13e
SebasGomez <sebas165019c@gmail.com>
2026-04-30 18:50:08 -0500

    commit se arreglan detalles, y se termina el programa

--- CALCULADORA.c
+++ CALCULADORA.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <math.h> 
+#include <math.h> //Para operaciones matematicas como potencia
+#include <windows.h>  // Para Sleep()
 
 int main (){
     //Declaracion de variables
@@ -9,6 +10,7 @@ int main (){
     //Menu de opciones
     do {
         printf("\n--------------------CALCULADORA EN C--------------------\n\n"); 
+        printf("Bienvenido a la calculadora en C, elija una de las siguientes opciones:\n\n"); 
         printf("0. Salir\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Potencia\n6. Residuo de una division\n7. Compare los numeros\n8. Mostrar numeros entre un intervalo\n");  
         printf("\nElija la operacion que desea realizar: ");
         scanf("%d", &op); 
@@ -21,6 +23,7 @@ int main (){
                 printf("Ingrese el segundo numero: ");
                 scanf("%d", &num2);
                 printf("El resultado de la suma es: %d\n", num1 + num2);
+                Sleep(1500);
                 break;
             case 2: //Resta
                 printf("Ingrese el primer numero: ");
@@ -28,6 +31,7 @@ int main (){
                 printf("Ingrese el segundo numero: ");
                 scanf("%f", &f2);
                 printf("El resultado de la resta es: %.2f\n", f1 - f2);
+                Sleep(1500);
                 break;
             case 3: //Multiplicacion
                 printf("Ingrese el primer numero: ");   
@@ -35,6 +39,7 @@ int main (){
                 printf("Ingrese el segundo numero: ");
                 scanf("%f", &f2);
                 printf("El resultado de la multiplicacion es: %.2f\n", f1 * f2);
+                Sleep(1500);
                 break;
             case 4: //Division
                 printf("Ingrese el primer numero: ");
@@ -46,6 +51,7 @@ int main (){
                     } else {
                         printf("Error: No se puede dividir por cero.\n");
                     }
+                Sleep(1500);
                 break;
             case 5: //Potencia
                 printf("Ingrese la base: ");    
@@ -53,7 +59,8 @@ int main (){
                 printf("Ingrese el exponente: ");
                 scanf("%f", &f2);
                 printf("El resultado de la potencia es: %.2f\n", pow(f1, f2));
-                break;
+                Sleep(1500);
+                break;            
             case 6: //Residuo de una division
                     printf("Ingrese el primer numero: ");
                     scanf("%d", &num1);
@@ -64,6 +71,7 @@ int main (){
                     } else {
                         printf("Error: No se puede dividir por cero.\n");
                     }
+                Sleep(1500);
                 break;
             case 7: //Comparar los numeros
                 printf("Ingrese el primer numero: ");
@@ -77,6 +85,7 @@ int main (){
                     } else {
                         printf("Ambos numeros son iguales.\n");
                     }
+                Sleep(1500);
                 break;
             case 8: //Mostrar numeros entre un intervalo incluyendo los números del intervalo
                 printf("Ingrese el primer numero: ");
@@ -98,8 +107,10 @@ int main (){
                     } else {
                         printf("Ingrese dos numeros diferentes para mostrar el intervalo.\n");
                     }
+                Sleep(1500);
                 break;
         } 
     }while (op != 0 || op < 0 || op > 8);
+    Sleep(1500);
     return 0; 
 }
