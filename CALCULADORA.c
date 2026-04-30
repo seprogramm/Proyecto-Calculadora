e74ab902aca292eb50f363fb5ac13e9414f2a792
SebasGomez <sebas165019c@gmail.com>
2026-04-30 18:11:28 -0500

    commit se muestra el menu hasta que el usuario decida

--- CALCULADORA.c
+++ CALCULADORA.c
@@ -1,39 +1,52 @@
 #include <stdio.h>
 
-void main (void){
+int main (){
     //Declaracion de variables
     int num1, num2, op; 
     float f1, f2; 
 
     //Menu de opciones
-    printf("\n--------------------CALCULADORA EN C--------------------\n\n"); 
-    printf("1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Potencia\n6. Residuo de una division\n7. Compare los numeros\n8. Mostrar numeros entre un intervalo\n");  
-    printf("\nElija la operacion que desea realizar: ");
-    scanf("%d", &op); 
-    fflush(stdin); 
-
-    switch (op)
-    {
-    case 1:
-        printf("Ingrese el primer numero: ");
-        scanf("%d", &num1);
-        printf("Ingrese el segundo numero: ");
-        scanf("%d", &num2);
-        printf("El resultado de la suma es: %d\n", num1 + num2);
-        break;
-    case 2:
-        printf("Ingrese el primer numero: ");
-        scanf("%f", &f1);
-        printf("Ingrese el segundo numero: ");
-        scanf("%f", &f2);
-        printf("El resultado de la resta es: %.2f\n", f1 - f2);
-        break;
-    case 3:
-        printf("Ingrese el primer numero: ");   
-        scanf("%f", &f1);
-        printf("Ingrese el segundo numero: ");
-        scanf("%f", &f2);
-        printf("El resultado de la multiplicacion es: %.2f\n", f1 * f2);
-        break;
-    }
+    do {
+        printf("\n--------------------CALCULADORA EN C--------------------\n\n"); 
+        printf("0. Salir\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Potencia\n6. Residuo de una division\n7. Compare los numeros\n8. Mostrar numeros entre un intervalo\n");  
+        printf("\nElija la operacion que desea realizar: ");
+        scanf("%d", &op); 
+        fflush(stdin);  
+    
+        switch (op) {
+            case 1:
+            printf("Ingrese el primer numero: ");
+            scanf("%d", &num1);
+            printf("Ingrese el segundo numero: ");
+            scanf("%d", &num2);
+            printf("El resultado de la suma es: %d\n", num1 + num2);
+            break;
+            case 2:
+            printf("Ingrese el primer numero: ");
+            scanf("%f", &f1);
+            printf("Ingrese el segundo numero: ");
+            scanf("%f", &f2);
+            printf("El resultado de la resta es: %.2f\n", f1 - f2);
+            break;
+            case 3:
+            printf("Ingrese el primer numero: ");   
+            scanf("%f", &f1);
+            printf("Ingrese el segundo numero: ");
+            scanf("%f", &f2);
+            printf("El resultado de la multiplicacion es: %.2f\n", f1 * f2);
+            break;
+            case 4:
+            printf("Ingrese el primer numero: ");
+            scanf("%f", &f1);
+            printf("Ingrese el segundo numero: ");
+            scanf("%f", &f2);
+                if (f2 != 0) {
+                    printf("El resultado de la division es: %.2f\n", f1 / f2);
+                } else {
+                    printf("Error: No se puede dividir por cero.\n");
+                }
+            break;
+        } 
+    }while (op != 0);
+    return 0; 
 }
